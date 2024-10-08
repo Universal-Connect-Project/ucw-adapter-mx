8e0d97de5be532a58c2158135e8134251ae1573d
Tyson Phalp <tyson.phalp@mx.com>
2024-10-08 16:05:31 -0600

    Tweak config for adapter

--- packages/mx-adapter/src/config.ts
+++ packages/mx-adapter/src/config.ts
@@ -1,35 +1,30 @@
 import * as dotenv from "dotenv";
 
 export const init = (path = ".env") => {
+  let envs: Record<string, string> = {};
+
   const result: dotenv.DotenvConfigOutput = dotenv.config({
     path
   });
 
   if (result.error) {
     throw result.error;
-  }
-
-  const { parsed: envs } = result;
-
-  // Validate envs
-  if (!envs?.HOSTURL) {
-    throw new Error("Missing HOSTURL. Check README.md and `../.env.example` for more info.");
-  }
-
-  if (!envs?.MXCLIENTID && !envs?.MXCLIENTIDPROD) {
-    throw new Error("Missing MXCLIENTID or MXCLIENTIDPROD. Check README.md and `../.env.example` for more info.");
-  }
-
-  if (!envs?.MXAPISECRET && !envs?.MXAPISECRETPROD) {
-    throw new Error("Missing MXAPISECRET or MXAPISECRETPROD. Check README.md and `../.env.example` for more info.");
+  } else {
+    envs = result.parsed || {
+      HOSTURL: process.env?.HOSTURL,
+      MXCLIENTID: process.env?.MXCLIENTID,
+      MXAPISECRET: process.env?.MXAPISECRET,
+      MXCLIENTIDPROD: process.env?.MXCLIENTIDPROD,
+      MXAPISECRETPROD: process.env?.MXAPISECRETPROD
+    };
   }
 
   return {
-    HOSTURL: envs?.HOSTURL,
-    MXCLIENTID: envs?.MXCLIENTID,
-    MXAPISECRET: envs?.MXAPISECRET,
-    MXCLIENTIDPROD: envs?.MXCLIENTIDPROD,
-    MXAPISECRETPROD: envs?.MXAPISECRETPROD
+    HOSTURL: envs?.HOSTURL || process.env?.HOSTURL,
+    MXCLIENTID: envs?.MXCLIENTID || process.env?.MXCLIENTID,
+    MXAPISECRET: envs?.MXAPISECRET || process.env?.MXAPISECRET,
+    MXCLIENTIDPROD: envs?.MXCLIENTIDPROD || process.env?.MXCLIENTIDPROD,
+    MXAPISECRETPROD: envs?.MXAPISECRETPROD || process.env?.MXAPISECRETPROD
   };
 };
 
