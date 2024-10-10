a0e35cb16d3d127e63f307aff13f76513983f1ed
Tyson Phalp <tyson.phalp@mx.com>
2024-10-10 16:05:05 -0600

    Fix playwright command

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
     }
   ],
   webServer: {
-    command: "npm run build",
+    command: "npm run dev:e2e",
     url: "http://127.0.0.1:8080",
     reuseExistingServer: !process.env.CI
   }
